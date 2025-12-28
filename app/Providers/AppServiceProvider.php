e4ff8efaa50f5cd1ee0507aaa4058c1985f38ab9
si-rekam-medis <lailymaghfira@gmail.com>
2025-12-28 18:16:47 +0700

    Fix providers for Railway deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,25 +2,17 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Login;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
-    protected $listen = [
-        Login::class => [
-            // listener kosong, pakai closure
-        ],
-    ];
-
-    public function boot(): void
+    public function register()
     {
-        parent::boot();
+        //
+    }
 
-        \Event::listen(Login::class, function ($event) {
-            $event->user->update([
-                'last_login_at' => now(),
-            ]);
-        });
+    public function boot()
+    {
+        //
     }
 }
